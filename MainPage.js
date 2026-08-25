f311a1802650580971cf6cdfc5c5d3f586cc52d7
GianmarcoTheDeveloper <gamergs02@gmail.com>
2026-08-25 14:25:12 -0400

    added moving the indicator logic back

--- MainPage.js
+++ MainPage.js
@@ -1,26 +1,40 @@
 const list = document.querySelectorAll('.navigation li');
 const indicator = document.querySelector('.indicator');
 
+function moveIndicator(li) {
+    const icon = li.querySelector('.icon');
+
+    // Get the position of the icon and indicator
+    const iconRect = icon.getBoundingClientRect();
+    const indicatorRect = indicator.getBoundingClientRect();
+
+    // Find the vertical center of the icon
+    const iconCenter = iconRect.top + (iconRect.height / 2);
+
+    // Find the vertical center of the indicator
+    const indicatorCenter = indicatorRect.top + (indicatorRect.height / 2);
+
+    // Calculate the difference
+    const difference = iconCenter - indicatorCenter;
+
+    // Move the indicator
+    indicator.style.transform = `translateY(${difference}px)`;
+}
+
 function activeLink() {
-    // Remove active from all links
     list.forEach((li) => {
         li.classList.remove('active');
     });
 
-    // Add active to clicked link
     this.classList.add('active');
 
-    // Calculate the position of the clicked item
-    const position = this.offsetTop - list[0].offsetTop;
-
-    // Move the indicator
-    indicator.style.transform = `translateY(${position}px)`;
+    moveIndicator(this);
 }
 
 list.forEach((li) => {
     li.addEventListener('click', activeLink);
 });
 
-// Set Home as the starting position
+// Start on Home
 list[0].classList.add('active');
-indicator.style.transform = 'translateY(0px)';
+moveIndicator(list[0]);
