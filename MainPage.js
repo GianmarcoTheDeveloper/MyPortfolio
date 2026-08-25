3a7ace15b9d3846a3130f86dbc5eecccc0c72217
GianmarcoTheDeveloper <gamergs02@gmail.com>
2026-08-25 14:16:36 -0400

    redoing functionality for slider

--- MainPage.js
+++ MainPage.js
@@ -1,16 +1,29 @@
-const $list = document.querySelectorAll('.navigation li');
+const list = document.querySelectorAll('.navigation li');
+const indicator = document.querySelector('.indicator');
 
-function activeLink()
-{
-    $list.forEach(($li) => {
-        $li.classList.remove('active') 
-    });
-    this.classList.add('active');
+function moveIndicator(item) {
+    const itemTop = item.offsetTop;
+    const firstItemTop = list[0].offsetTop;
+
+    const y = itemTop - firstItemTop;
+
+    indicator.style.transform = `translateY(${y}px)`;
 }
 
-$list.forEach(($li) =>{
-    $li.addEventListener(
-        'click',
-        activeLink,
-    );
+function activeLink() {
+    list.forEach((li) => {
+        li.classList.remove('active');
+    });
+
+    this.classList.add('active');
+
+    moveIndicator(this);
+}
+
+list.forEach((li) => {
+    li.addEventListener('click', activeLink);
 });
+
+// Start with the first item selected
+list[0].classList.add('active');
+moveIndicator(list[0]);
