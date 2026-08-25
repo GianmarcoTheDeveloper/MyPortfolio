7476f4f57297dac44a453406ff6e3b07e0dde994
GianmarcoTheDeveloper <gamergs02@gmail.com>
2026-08-25 14:20:57 -0400

    modified the moving of the indicator
    indicator wasn't moving so i modified how it moves
    
    made sure home is the first indicator

--- MainPage.js
+++ MainPage.js
@@ -1,29 +1,26 @@
 const list = document.querySelectorAll('.navigation li');
 const indicator = document.querySelector('.indicator');
 
-function moveIndicator(item) {
-    const itemTop = item.offsetTop;
-    const firstItemTop = list[0].offsetTop;
-
-    const y = itemTop - firstItemTop;
-
-    indicator.style.transform = `translateY(${y}px)`;
-}
-
 function activeLink() {
+    // Remove active from all links
     list.forEach((li) => {
         li.classList.remove('active');
     });
 
+    // Add active to clicked link
     this.classList.add('active');
 
-    moveIndicator(this);
+    // Calculate the position of the clicked item
+    const position = this.offsetTop - list[0].offsetTop;
+
+    // Move the indicator
+    indicator.style.transform = `translateY(${position}px)`;
 }
 
 list.forEach((li) => {
     li.addEventListener('click', activeLink);
 });
 
-// Start with the first item selected
+// Set Home as the starting position
 list[0].classList.add('active');
-moveIndicator(list[0]);
+indicator.style.transform = 'translateY(0px)';
